646ca2312bc48eb231f6800d76cdd028d3277349
malaimo <malaimo@gmail.com>
2012-02-17 07:12:06 -0300

    Modelado y Carga de Paises

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120213114658) do
+ActiveRecord::Schema.define(:version => 20120217100107) do
+
+  create_table "countries", :force => true do |t|
+    t.string   "name"
+    t.string   "iso_code"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
