3b80704974afe7688afa28bcb24b26ef0b0da115
Martin Alaimo <malaimo@gmail.com>
2012-08-20 15:38:36 -0400

    Revert "Revert "La duración de los eventos a novel de tipo de evento.""
    This reverts commit c49f69ee1176ce9f95ed048865f89915aa9fd969.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120820142837) do
+ActiveRecord::Schema.define(:version => 20120820165113) do
 
   create_table "countries", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120820142837) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.text     "goal"
+    t.integer  "duration"
   end
 
   create_table "events", :force => true do |t|
--- spec/views/event_types/index.html.haml_spec.rb
+++ spec/views/event_types/index.html.haml_spec.rb
@@ -7,12 +7,14 @@ describe "event_types/index" do
     assign(:event_types, [
       stub_model(EventType,
         :name => "Tipo de evento 1",
+        :duration => 8,
         :description => "La descripción del tipo de evento 1",
         :recipients => "Los destinatarios del tipo de evento 1",
         :program => "El programa del tipo de evento 1"
       ),
       stub_model(EventType,
         :name => "Tipo de evento 2",
+        :duration => 8,
         :description => "La descripción del tipo de evento 2",
         :recipients => "Los destinatarios del tipo de evento 2",
         :program => "El programa del tipo de evento 2"
@@ -28,7 +30,5 @@ describe "event_types/index" do
     assert_select "tr>td", :text => "Tipo de evento 2".to_s, :count => 1
     assert_select "tr>td", :text => "La descripción del tipo de evento 1".to_s, :count => 1
     assert_select "tr>td", :text => "La descripción del tipo de evento 2".to_s, :count => 1
-    assert_select "tr>td", :text => "Los destinatarios del tipo de evento 1".to_s, :count => 1
-    assert_select "tr>td", :text => "Los destinatarios del tipo de evento 2".to_s, :count => 1
   end
 end
