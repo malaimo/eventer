2a6d92018fcba3fabe5704bf5b6bd461a3d8b4af
Juan Gabardini <jgabardini@computer.org>
2013-04-01 17:11:23 -0300

    calculate how many week ahead a event is

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   belongs_to :trainer
   belongs_to :event_type
   has_many :participants
-  
+
   scope :visible, where(:cancelled => false).where("date >= ?", DateTime.now)
   scope :public_events,  where("visibility_type = 'pu' or visibility_type = 'co'")
   scope :public_commercial_events,  where(:visibility_type => "pu")
@@ -11,48 +11,48 @@ class Event < ActiveRecord::Base
   scope :public_commercial_visible, self.visible.public_commercial_events
   scope :public_community_visible, self.visible.public_community_events
   scope :public_and_visible, self.visible.public_events
-  
+
   after_initialize :initialize_defaults
-  
-  attr_accessible :event_type_id, :trainer_id, :country_id, :date, :place, :capacity, :city, :visibility_type, :list_price, 
-                  :list_price_plus_tax, :list_price_2_pax_discount, :list_price_3plus_pax_discount, 
+
+  attr_accessible :event_type_id, :trainer_id, :country_id, :date, :place, :capacity, :city, :visibility_type, :list_price,
+                  :list_price_plus_tax, :list_price_2_pax_discount, :list_price_3plus_pax_discount,
                   :eb_price, :eb_end_date, :draft, :cancelled, :registration_link, :is_sold_out, :participants
-                  
-  validates :date, :place, :capacity, :city, :visibility_type, :list_price, 
+
+  validates :date, :place, :capacity, :city, :visibility_type, :list_price,
             :country, :trainer, :event_type, :presence => true
-  
+
   validates :capacity, :numericality => { :greater_than => 0, :message => :capacity_should_be_greater_than_0 }
-  
+
   validates_each :date do |record, attr, value|
     record.errors.add(attr, :event_date_in_past) unless !value.nil? && value > Time.zone.today
   end
-  
+
   validates_each :eb_end_date do |record, attr, value|
       record.errors.add(attr, :eb_end_date_should_be_earlier_than_event_date) unless value.nil? || value < record.date
   end
-  
+
   validates_each :eb_price do |record, attr, value|
       record.errors.add(attr, :eb_price_should_be_smaller_than_list_price) unless value.nil? || value < record.list_price
   end
-  
+
   validates_each :list_price_2_pax_discount  do |record, attr, value|
     if !value.nil? && (value > 0 && record.visibility_type == 'pr')
-      record.errors.add(attr, :private_event_should_not_have_discounts) 
+      record.errors.add(attr, :private_event_should_not_have_discounts)
     end
   end
-  
+
   validates_each :list_price_3plus_pax_discount  do |record, attr, value|
     if !value.nil? && (value > 0 && record.visibility_type == 'pr')
-      record.errors.add(attr, :private_event_should_not_have_discounts) 
+      record.errors.add(attr, :private_event_should_not_have_discounts)
     end
   end
-  
+
   def initialize_defaults
     if new_record?
 #      self.draft = true
     end
   end
-  
+
   def completion
     if self.capacity > 0
       self.participants.confirmed.count*1.0/self.capacity
@@ -60,5 +60,14 @@ class Event < ActiveRecord::Base
       1.0
     end
   end
+
+  def weeks_from(now=Date.today)
+    from_date = now.beginning_of_week
+    to_date = self.date.beginning_of_week
+
+    weeks = (to_date - from_date) / 7
+  end
+
+
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,103 +1,103 @@
 require 'spec_helper'
 
 describe Event do
-  
+
   before(:each) do
     @event = FactoryGirl.build(:event)
   end
-  
+
   it "should be valid" do
     @event.valid?.should be true
   end
-    
+
   it "should require its date" do
     @event.date = ""
-    
+
     @event.valid?.should be false
   end
-  
+
   it "should require its place" do
     @event.place = ""
-    
+
     @event.valid?.should be false
   end
-  
+
   it "should require its city" do
     @event.city = ""
-    
+
     @event.valid?.should be false
   end
-  
+
   it "should require its visibility_type" do
     @event.visibility_type = ""
-    
+
     @event.valid?.should be false
   end
-  
+
   it "should require its list_price" do
     @event.list_price = ""
-    
+
     @event.valid?.should be false
   end
-    
+
   it "should require its country" do
     @event.country = nil
-    
+
     @event.valid?.should be false
   end
-  
+
   it "should require its trainer" do
     @event.trainer = nil
-    
+
     @event.valid?.should be false
   end
-  
+
   it "should require its event_type" do
     @event.event_type = nil
-    
+
     @event.valid?.should be false
   end
-  
+
   it "should have a future date" do
       @event.date = "31/01/2000"
 
       @event.valid?.should be false
   end
-  
+
   it "should have a capacity greater than 0" do
       @event.capacity = 0
 
       @event.valid?.should be false
   end
-  
+
   it "Early Bird price should be smaller than List Price" do
       @event.list_price = 100
       @event.eb_price = 200
-      
+
       @event.valid?.should be false
   end
-  
+
   it "Early Bird date should be earlier than Event date" do
       @event.date = "31/01/3000"
       @event.eb_end_date = "31/01/3100"
 
       @event.valid?.should be false
   end
-  
+
   it "A private event should not have discounts for 2 persons" do
        @event.visibility_type = "pr"
        @event.list_price_2_pax_discount = 10
 
        @event.valid?.should be false
    end
-   
+
    it "A private event should not have discounts for 3+ persons" do
         @event.visibility_type = "pr"
         @event.list_price_3plus_pax_discount = 10
 
         @event.valid?.should be false
     end
-    
+
     it "A public event can have discounts" do
          @event.visibility_type = 'pu'
          @event.list_price_2_pax_discount = 10
@@ -105,17 +105,30 @@ describe Event do
 
          @event.valid?.should be true
      end
-     
+
      it "It should return a completion percentage" do
-       
+
           @event.capacity = 10
-          
+
           p = Participant.new({:fname => "juan", :lname => "pipo", :phone => "1234-5678", :email => "ppp@ppp.com"})
           p.event = @event
           p.status = "C"
           p.save
-          
+
           @event.completion.should == 0.1
       end
-  
+
+     it "It should compute weeks from now" do
+          today = Date.today
+          @event.date = today
+
+          @event.weeks_from(today.weeks_ago(3)).should == 3
+      end
+
+     it "It should compute weeks from now (next year)" do
+          today = Date.today
+          @event.date = today.years_since(1)
+
+          @event.weeks_from(today).should == 52
+      end
 end
