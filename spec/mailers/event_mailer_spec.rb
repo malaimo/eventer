ddd6836839ad726deb97ff72e9c6b2fab88a7c64
Carlos Peix <carlos.peix@gmail.com>
2014-01-22 16:58:19 -0300

    should send early bird prices if available

--- spec/mailers/event_mailer_spec.rb
+++ spec/mailers/event_mailer_spec.rb
@@ -32,4 +32,32 @@ describe EventMailer do
     html_message = email.body.parts.find {|p| p.content_type.match /html/}.body.raw_source
     html_message.should include("(ARS) $ 200.00")
   end
+
+  it "should not send early bird prices EB info is empty" do
+    participant = FactoryGirl.build(:participant)
+    participant.email = "carlos.peix@kleer.la"
+    participant.event.list_price = 200
+
+    email = EventMailer.welcome_new_event_participant(participant).deliver
+
+    text_message = email.body.parts.find {|p| p.content_type.match /plain/}.body.raw_source
+    text_message.should_not include("Si pagas antes del")
+    html_message = email.body.parts.find {|p| p.content_type.match /html/}.body.raw_source
+    html_message.should_not include("Si pagas antes del")
+  end
+
+  it "should send early bird prices EB info is available" do
+    participant = FactoryGirl.build(:participant)
+    participant.email = "carlos.peix@kleer.la"
+    participant.event.list_price = 200
+    participant.event.eb_end_date = Date.today
+    participant.event.eb_price = 180
+
+    email = EventMailer.welcome_new_event_participant(participant).deliver
+
+    text_message = email.body.parts.find {|p| p.content_type.match /plain/}.body.raw_source
+    text_message.should include("(ARS) $ 180.00")
+    html_message = email.body.parts.find {|p| p.content_type.match /html/}.body.raw_source
+    html_message.should include("(ARS) $ 180.00")
+  end
 end
