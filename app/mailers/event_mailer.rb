5df25f50a4104e73d721457967ee0835049e145e
Martin Alaimo <malaimo@gmail.com>
2014-05-17 20:38:46 -0500

    Notification E-Mail Enhacement

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -26,8 +26,8 @@ class EventMailer < ActionMailer::Base
     if !@participant.event.monitor_email.nil? && @participant.event.monitor_email != ""
       mail(to: @participant.event.monitor_email, 
           from: "Eventos <eventos@kleerer.com>", 
-          subject: "[Keventer] Nuevo registro a #{@participant.event.event_type.name}: #{@participant.fname} #{@participant.lname}",
-          body: "Una nueva persona se registró a #{@participant.event.event_type.name}. Puedes ver/editar el registro en #{edit_registration_link}.")
+          subject: "[Keventer] Nuevo registro a #{@participant.event.event_type.name} en #{@participant.event.country.name}: #{@participant.fname} #{@participant.lname}",
+          body: "Una nueva persona se registró a #{@participant.event.event_type.name} del #{@participant.event.human_date} en #{@participant.event.country.name}. Puedes ver/editar el registro en #{edit_registration_link}.")
     end
   end
 
