3cc9999745364dc154db884dc4d15697226c4f57
Juan Gabardini <jgabardini@computer.org>
2014-09-30 23:58:23 -0300

    a couple of test of views added

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,5 +1,54 @@
 require 'spec_helper'
 
 describe "home/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    assign(:events, [
+      stub_model(Event,
+        :event_type => FactoryGirl.build(:event_type),
+        :place => "Place",
+        :date => "2015-01-01",
+        :capacity => 20,
+        :city => "City",
+        :country => FactoryGirl.build(:country),
+        :trainer => FactoryGirl.build(:trainer),
+        :visibility_type => "Type",
+        :list_price => "9.99",
+        :list_price_plus_tax => false,
+        :list_price_2_pax_discount => 15,
+        :list_price_3plus_pax_discount => 25,
+        :eb_price => "19.99",
+        :draft => true,
+        :cancelled => false,
+        :is_webinar => false
+      ),
+      stub_model(Event,
+        :event_type => FactoryGirl.build(:event_type),
+        :place => "Place",
+        :date => "2015-01-01",
+        :capacity => 20,
+        :city => "City",
+        :country => FactoryGirl.build(:country),
+        :trainer => FactoryGirl.build(:trainer),
+        :visibility_type => "Type",
+        :list_price => "9.99",
+        :list_price_plus_tax => false,
+        :list_price_2_pax_discount => 15,
+        :list_price_3plus_pax_discount => 25,
+        :eb_price => "19.99",
+        :draft => false,
+        :cancelled => false,
+        :is_webinar => false
+      )
+    ])
+  end
+
+  it "renders a list of events" do
+    I18n.locale = :es
+    render
+    assert_select "tr>th", :text => "Fecha", :count => 1
+    assert_select "tr>th", :text => "Nombre", :count => 1
+ 
+    assert_select "tr>td", :text => "2015-01-01", :count => 2
+  end
+
 end
